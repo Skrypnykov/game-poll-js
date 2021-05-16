32c77818a0719443b79931bdc3fd8ea6c556a7cc
Oleh Skrypnykov <oskripnikov@gmail.com>
2021-05-16 21:25:23 +0300

    update reg form

--- js/register.js
+++ js/register.js
@@ -1,7 +1,7 @@
 ## Register user
 
 url = 'https://pollgame-be.herokuapp.com/users/'
-userData = {nikName:"2nikName",fullName:"2fullName",organization:"2organization",position:"2position",email:"mail@mail.com",phone:"+380953585421",password:"147258369"}
+userData = {nikName:"2nikName",fullName:"2fullName",organization:"2organization",position:"2position",email:"nikname@gmail.com",phone:"+38 (095) 358-54-21",password:"147258369"}
 
 async function registerUser(url, userData) {
   const requestProp = {
