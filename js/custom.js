1b4611eb791fdb442b1c95f1f827df9d22686fa5
dzmitryNz <d.nayezzhy@gmail.com>
2021-05-22 19:14:41 +0300

    feat: signin signout refact

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,27 @@
-// import { register, signIn } from "./users.js";
-import "./questions.js";
+import { verifyAuth, register, signIn } from "./users.js";
 
 const registerForm = document.getElementById("signUpForm");
 const signInForm = document.getElementById("signInForm");
+const signInOutButton = document.getElementById("signInOutButton");
 
-// registerForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   const formArray = Array.from(event.target);
-//   let userData = {};
-//   formArray.forEach((value) => {
-//     userData[value.id] = value.value;
-//   });
-//   register(userData);
-// });
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formArray = Array.from(event.target);
+  let userData = {};
+  formArray.forEach((value) => {
+    userData[value.id] = value.value;
+  });
+  register(userData);
+});
 
-// signInForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   const formArray = Array.from(event.target);
-//   let userData = {};
-//   formArray.forEach((value) => {
-//     userData[value.id] = value.value;
-//   });
-//   signIn(userData);
-// });
+signInForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formArray = Array.from(event.target);
+  let userData = {};
+  formArray.forEach((value) => {
+    userData[value.name] = value.value;
+  });
+  signIn(userData);
+});
 
-// questions();
+verifyAuth();
