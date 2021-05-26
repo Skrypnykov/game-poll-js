4210d43336ec404b435c3ef4b124aa75d5af0b00
Saylance2503 <yurik_05@inbox.ru>
2021-05-26 22:11:18 +0400

    feat: fix timer

--- js/questions.js
+++ js/questions.js
@@ -1,22 +1,23 @@
 import { apiGet } from "./getData.js";
 import create from "./create.js";
 import { URL, questionMax } from "./constants.js";
+import start from "./timer.js";
 
 const questionsQuantity = document.getElementById("questionsQuantity"),
-      questionsScore = document.getElementById("questionsScore"),
-      questionBlock = document.getElementById("questionBlock"),
-      answersBlock = document.getElementById("answersBlock"),
-      headerBlock = document.querySelector(".header-wrapper"),
-      questionText = document.getElementById("question"),
-      highlighting = document.querySelector(".item-block1"),
-      goals = document.querySelector(".goals"),
-      bg = document.querySelector(".page"),
-      fullUrl = URL + "questions/all";
+  questionsScore = document.getElementById("questionsScore"),
+  questionBlock = document.getElementById("questionBlock"),
+  answersBlock = document.getElementById("answersBlock"),
+  headerBlock = document.querySelector(".header-wrapper"),
+  questionText = document.getElementById("question"),
+  goals = document.querySelector(".goals"),
+  bg = document.querySelector(".page"),
+  fullUrl = URL + "questions/all";
 
 let questionNum = 1,
-    scores = 0,
-    trueAnswer = "",
-    questionsArr = [];
+  scores = 0,
+  trueAnswer = "",
+  questionsArr = [];
+let elem = document.getElementById("timer");
 
 async function handler() {
   apiGet(fullUrl).then((responseData) => {
@@ -41,6 +42,8 @@ const nextQuestion = () => {
     questionNum = questionNum + 1;
     questionText.innerText = "";
     answersBlock.innerText = "";
+    elem.value = 90;
+    start(elem.value);
     setQuestion();
   }
 };
@@ -57,7 +60,7 @@ const answerIsWrong = (target) => {
 };
 
 const verifyAnswer = (target) => {
-  if(!Array.isArray(target)) {
+  if (!Array.isArray(target)) {
     const answer = target.innerText;
     console.log(answer);
     if (answer === trueAnswer) {
@@ -75,7 +78,7 @@ const verifyAnswer = (target) => {
       setTimeout(() => answerIsWrong(), 1000);
       console.log("Wrong");
     }
-  };
+  }
 };
 
 const questionsQuantityUpdate = () => {
@@ -194,34 +197,34 @@ const setRange = (answers) => {
   btnSkip.addEventListener("click", () => answerIsWrong());
 
   const verifyRange = () => {
-
     let sms = "";
     let enter = document.getElementById("enter");
 
-    if (p.innerHTML === "") { //проверка на то, что ползунок не двигали
-      show('1', 'visible'); //вывод модального окна
-    } else {//если ползунок двигали, сравниваем его значение с нижней и верхней границей
-    
-    if ((rng.valueAsNumber >= nd) && (rng.valueAsNumber <= nu)) {
-      enter.style.backgroundColor = "rgba(76, 161, 70, 0.6)";//правильный ответ - зеленая кнопка
+    if (p.innerHTML === "") {
+      //проверка на то, что ползунок не двигали
+      show("1", "visible"); //вывод модального окна
     } else {
-      enter.style.backgroundColor = "rgba(229, 35, 61, 0.6)";//неправильный ответ - красная кнопка
+      //если ползунок двигали, сравниваем его значение с нижней и верхней границей
+
+      if (rng.valueAsNumber >= nd && rng.valueAsNumber <= nu) {
+        enter.style.backgroundColor = "rgba(76, 161, 70, 0.6)"; //правильный ответ - зеленая кнопка
+      } else {
+        enter.style.backgroundColor = "rgba(229, 35, 61, 0.6)"; //неправильный ответ - красная кнопка
+      }
+      sms = nr + ed + " (від " + nd + " до " + nu + ")"; //правильный ответ
+      enter.innerHTML = sms; //вывод правильного ответа в кнопку
     }
-    sms = nr + ed + " (від " + nd + " до " + nu + ")";//правильный ответ
-    enter.innerHTML = sms;//вывод правильного ответа в кнопку
-  }
-  }; 
+  };
 };
 
 function show(op, vis) {
   document.getElementById("rangeModal").style.opacity = op;
   document.getElementById("rangeModal").style.visibility = vis;
-  
 }
 
 const setList = (answers) => {
   let answer;
-  
+
   const btnSkip = create(
     "button",
     "i3 item3",
@@ -236,27 +239,46 @@ const setList = (answers) => {
   answers.forEach((answer, i) => {
     const s = "s" + i;
     const label = create("label", "selectLabel", answer, null, ["for", s]);
-    const input = create("input",
+    const input = create(
+      "input",
       "selectInput",
       answer,
       null,
-      ["id", s], ["type", "checkbox"], ["value", answer], ["name", "selectAnsw"]
+      ["id", s],
+      ["type", "checkbox"],
+      ["value", answer],
+      ["name", "selectAnsw"]
     );
     const selectDiv = create("div", "selectDiv", [label, input], null);
     selectDivs.push(selectDiv);
   });
 
-  const listItem1 = create("button",
+  const listItem1 = create(
+    "button",
     "list-item1",
     "Оберіть варіанти",
     null,
-    ["type", "button"], ["id", "btnList", "onclick", "listshow()"]);
+    ["type", "button"],
+    ["id", "btnList", "onclick", "listshow()"]
+  );
 
   const arrow = create("div", "arrow", null, null, ["onclick", "listshow()"]);
   const selectDivCommon = create("div", "selectDivCommon", selectDivs, null);
-  const scrollSelect = create("div", "scrollSelect hover", selectDivCommon, null, ["id", "dropup"]);
-  const listup = create("form", "listup", [listItem1, scrollSelect, arrow], null, ["id", "listup"]);
-    const btnEnter = create(
+  const scrollSelect = create(
+    "div",
+    "scrollSelect hover",
+    selectDivCommon,
+    null,
+    ["id", "dropup"]
+  );
+  const listup = create(
+    "form",
+    "listup",
+    [listItem1, scrollSelect, arrow],
+    null,
+    ["id", "listup"]
+  );
+  const btnEnter = create(
     "button",
     "i2 item2",
     "Дати відповідь",
@@ -266,14 +288,25 @@ const setList = (answers) => {
     ["id", "enter"]
   );
   const listBlock = create("div", "list-block", [listup], null);
-  const itemBlock1 = create("div", "item-block1", [listBlock, btnEnter, btnSkip], null, ["id", "list"]);
-  const naviList = create("div", "three-buttons navi-list", [itemBlock1], answersBlock);
+  const itemBlock1 = create(
+    "div",
+    "item-block1",
+    [listBlock, btnEnter, btnSkip],
+    null,
+    ["id", "list"]
+  );
+  const naviList = create(
+    "div",
+    "three-buttons navi-list",
+    [itemBlock1],
+    answersBlock
+  );
   listup.addEventListener("submit", function (event) {
     event.preventDefault();
     const formArray = Array.from(event.target);
     let answer = [];
     formArray.forEach((value) => {
-      if(value.checked) answer.push(value.value);
+      if (value.checked) answer.push(value.value);
     });
     verifyAnswer(answer);
   });
@@ -289,3 +322,4 @@ const setList = (answers) => {
 };
 
 handler();
+export default answerIsWrong;
